5d7b6157165c1404d93956e0d34cb5297941a3f3
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2021-09-20 16:46:57 +0100

    Implement POST-Redirect-GET By Saving And Displaying Submitted Comments

--- Lesson-2/5_MessageboardPartThree/MessageboardPartThree.py
+++ Lesson-2/5_MessageboardPartThree/MessageboardPartThree.py
@@ -32,7 +32,7 @@ class MessageHandler(BaseHTTPRequestHandler):
         # Read the correct amount of data from the request.
         data = self.rfile.read(length).decode()
         # Extract the "message" field from the request data.
-        message = parse_qs(data)["message"][0]
+        message = parse_qs(data).get("message",[""])[0]
 
         # Escape HTML tags in the message so users can't break world+dog.
         message = message.replace("<", "&lt;")
@@ -41,6 +41,9 @@ class MessageHandler(BaseHTTPRequestHandler):
         memory.append(message)
 
         # 1. Send a 303 redirect back to the root page.
+        self.send_response(303)
+        self.send_header('Location', '/')
+        self.end_headers()
 
     def do_GET(self):
         # First, send a 200 OK response.
@@ -51,8 +54,10 @@ class MessageHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # 2. Put the response together out of the form and the stored messages.
+        res = form.format(memory)
 
         # 3. Send the response.
+        self.wfile.write(res.encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)
--- Lesson-2/README.md
+++ Lesson-2/README.md
@@ -11,3 +11,14 @@ There are eight downloadable exercises in Lesson 2 of this course.
 * The JSON client demo
 * The bookmark server
 
+## My Observations
+- The messageboard handles two POST request from my Firefox browser and saves two `message` similar string each time I interract with the submit button.
+    - Button clicks
+    - `Tab` and `enter`
+    - `Tab` and `spacebar`
+- The Firefox Debugger Network tab is reporting only one sent POST request and one received GET request.
+- I might troubleshoot this from tcpdump later on.(I'm kind of new to tcpdump)
+- Chrome behaves as expected.
+
+
+I also just noticed that changing the file permissions alerts git of a change in the tracked files or folders
