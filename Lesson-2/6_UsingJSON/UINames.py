506a27aa5fd84d78e1cb031cd01276e60c3ec444
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2022-02-07 05:15:03 +0100

    Request for names

--- Lesson-2/6_UsingJSON/UINames.py
+++ Lesson-2/6_UsingJSON/UINames.py
@@ -12,12 +12,16 @@ import requests
 
 
 def SampleRecord():
-    r = requests.get("http://uinames.com/api?ext&region=United%20States",
-                     timeout=2.0)
+    r = requests.get("http://uinames.com/api?ext&region=United%20States", # uinames API isn't working
+                     timeout=2.0)                                         # at the moment
     # 1. Add a line of code here to decode JSON from the response.
+    person = r.json()
 
     return "My name is {} {} and the PIN on my card is {}.".format(
         # 2. Add the correct fields from the JSON data structure.
+        person['name'],
+        person['surname'],
+        person['credit_card']['pin']
     )
 
 if __name__ == '__main__':
